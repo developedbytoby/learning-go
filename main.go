3b4b18a4af3820829006f55fcb2607c5a9d9c40f
Toby <mrtobez08@gmail.com>
2022-10-23 20:07:58 +0100

    more go!

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
-var foo = "bar"
+var (
+	hey bool = false
+)
 
 func main() {
-	fmt.Println("Here we go...", "\n", foo, rand.Intn(10))
+	i := 42
+	f := float64(i)
+	u := uint(f)
+	never := true
+	const rick = "yes"
+
+	fmt.Println(u, never, rick, hey)
+	helloWorld()
+}
+
+func helloWorld() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += 1
+	}
+	fmt.Println(sum)
 }
